8b6a499be79e8d86c61872fdb4179177c9056a98
Robel Mengistu <62121236+robelv2020@users.noreply.github.com>
2020-11-22 20:29:33 -0700

    action fetchSmurf

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
+import { connect } from 'react-redux'
+// import {fetchSmurfs} from '../actions'
+// import SmurfList from './smurfs/SmurfList';
+//import SmurFrom from './smurfs/SmurfForm';
+
 class App extends Component {
+  // array with one smurf returned 
+  state = {
+    url: `http://localhost:3333/smurfs`
+  };
+
+  componentDidMount() {
+    this.props.fetchSmurfs(this.state.url);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! W/Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
+        <h2> Smurfs village!</h2>
+        <SmurfList />
+        <SmurfForm />
+      </div >
     );
   }
 }
 
-export default App;
+export default Connect(null, { fetchSmurfs }(App);
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// import {}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
